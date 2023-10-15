f7503ea17069d1a9c9fb109dd8d5d7fccf175f38
ghyboba <91431321+ghyboba@users.noreply.github.com>
2023-10-15 22:37:14 +0300

    Create tikosoft.js

---
+++ tikosoft.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){$('.hamburger').on('click',function(){$('.subcategories,.supportmenu').hide();$('.menus').addClass('visible');$(this).hide();$('.close').show()}
+);$('.close').on('click',function(){$('.subcategories,.supportmenu').show();$('.menus').removeClass('visible');$('.hamburger').show();$(this).hide()}
+);$('.Search').on('click',function(){console.log('search Box opened');$('.subcategories').slideToggle();$('.subcategories').css('position','absolute');$('.subcategories').css('top','57px');$('.Header').css('height','57px');return false}
+)}
+);
